cf7907b1f7e3075edce5aefbd08100c9b6863eb1
IlinDenis <ilin.dv@svrauto.ru>
2023-07-04 17:03:05 +0300

    Initial commit

---
+++ pages/keycloack_auth_page.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import BasePage
+
+
+class keycloack_auth_form(BasePage):
+    NAME_BAR = (By.XPATH, '//input[@name="username"]')
+    PASSWORD_BAR = (By.XPATH, '//input[@name="password"]')
+    SUBMIT_BUTTON = (By.XPATH, '//button[@value="Submit"]')
+    FORGOT_PASS = (By.XPATH, '//span[contains(text(),"Забыли пароль?")]')
+    REMEMBER_ME = (By.XPATH, '//div[@name="rememberMe"]')
+    ERROR_MESSAGE = (By.XPATH, '//div[@class="sa-input__message"]')
+
+    def __init__(self, driver, url):
+        super().__init__(driver, url)
+
+    def login(self, email, password):
+        self.fill_text(self.NAME_BAR, email)
+        self.fill_text(self.PASSWORD_BAR, password)
+        self.click(self.SUBMIT_BUTTON)
+
+    def error_message(self):
+        wait = WebDriverWait(self.driver, 10)
+        phrase = wait.until(EC.visibility_of_element_located(self.ERROR_MESSAGE)).text
+        return phrase
---
+++ tests/keycloack_auth_test.py
@@ -0,0 +1,31 @@
+from pages.base_page import BasePage
+from pages.keycloack_auth_page import keycloack_auth_form
+
+CPurl = 'https://develop-cp.dev.svrauto.ru/auth/login'
+email = 'admin@svrauto.ru'
+wrongemail = 'admin@admin@ru23'
+wronguserpass = '123456789'
+userpass = 'adminPass'
+errortext = 'Неправильное имя пользователя или пароль.'
+
+
+def test_login_wrongPass(driver):
+    page = BasePage(driver, CPurl)
+    page.open()
+    auth_form = keycloack_auth_form(driver, url=CPurl)
+    auth_form.login(email, wronguserpass)
+    assert errortext == auth_form.error_message(), "Wrong error text"
+
+def test_login_wrongMail(driver):
+    page = BasePage(driver, CPurl)
+    page.open()
+    auth_form = keycloack_auth_form(driver, url=CPurl)
+    auth_form.login(wrongemail, userpass)
+    assert errortext == auth_form.error_message(), "Wrong error text"
+
+def test_login_correctUser(driver):
+    page = BasePage(driver, CPurl)
+    page.open()
+    auth_form = keycloack_auth_form(driver, url=CPurl)
+    auth_form.login(email, userpass)
+    assert 'Панель управления' == driver.title, "Wrong title of page, or wrong page was loaded"
